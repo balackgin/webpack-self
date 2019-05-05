4431ff5e2450c7a9140e7b51aad5dfae527132d2
chen_junyu <chen_junyu@dahuatech.com>
2019-05-05 16:26:51 +0800

    project initial

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtracTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('copy-webpack-plugin');
+const srcDir = path.join(__dirname, './src');
+const distDir = path.join(__dirname, './dist');
+
+module.exprots = {
+  entry: {
+    index: [
+      'webpack/hot/dev-server',
+      'webpack-dev-server/client?http://localhost:8001',
+      './src/js/index.js'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '../../',
+    filename: 'js[name].min.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /mode_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtracTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader'
+          }
+        })
+      },
+      {
+        test:/\/(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+        loader: 'url-loader?limit=8192&name=img/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ExtracTextPlugin('style/[name].min.css'),
+    new HtmlWebpackPlugin({
+      hash: true,
+      chunks: ['inedx'],
+      template: './src/pages/index/index.html',
+      filename: 'pages/index/index/html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
---
+++ webpack.dev.service.js
@@ -0,0 +1,28 @@
+var WebpackDevServer = require('webpack-dev-server');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var compiler = webpack(webpackConfig);
+var server = new WebpackDevServer(compiler, {
+  hot: true,
+  historyApiFallback: true,
+  // It suppress error shown in console, so it has to be set to false.
+  quiet: false,
+  // It suppress everything except error, so it has to be set to false as well
+  // to se success build.
+  noInfo: false,
+  stats: {
+    // Config for minimal console.log mess.
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false
+  },
+
+  // contentBase不要直接指向pages， 因为会导致css、js资源加载不到
+  contentBase: __dirname + '/src/'
+}).listen(8001, '0.0.0.0', function(err){
+  if(err) console.log(err);
+})
